2f10519b721c52350094917b04318164aca5c5e0
Danilo Santos <danilo.aparecido.santos@gmail.com>
2023-03-12 06:33:47 -0300

    exemplo de test que inicou com manual e virou uma automação via shell

--- testes_api_curl/get_administrador.sh
+++ testes_api_curl/get_administrador.sh
@@ -1,3 +1,8 @@
 echo "Digite o id do modelo"
 read id
 curl -H "Content-Type: application/json" -X GET http://localhost:5135/api/administradores/$id
+# caso você queira passar o token no header de uma api autenticada
+# curl -H "Content-Type: application/json" -H "Authorization: Bearer 12122121267117612852176125" -X GET http://localhost:5135/api/administradores/$id
+
+
+
